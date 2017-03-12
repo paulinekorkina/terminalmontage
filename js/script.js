e0e77522fa334b69eba9a8f6c6357166636d9204
polina.korkina <p.v.korkina@gmail.com>
2017-03-12 12:24:44 +0500

    Добавила плавную прокрутку к якорям по клику на иконку мыши

--- js/script.js
+++ js/script.js
@@ -30,3 +30,20 @@ overlay.addEventListener("click", function(event) {
   popup.classList.remove("modal-window-open");
   overlay.classList.remove("modal-overlay-open");
 });
+
+$(function() {
+  $('.smooth').bind('click',function(event){
+    var $anchor = $(this);
+
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500,'easeInOutExpo');
+        /*
+        если вы не хотите использовать эффект:
+        $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+          }, 1000);
+          */
+        event.preventDefault();
+  });
+});
